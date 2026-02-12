eb1ee152bd388c56850c3e36a41bfd149e6df5f6
keerthivarsha15607-commits <keerthivarsha15607@gmail.com>
2026-02-12 14:59:12 +0530

    Add files via upload

---
+++ OOPS LAB/Car.java
@@ -0,0 +1,15 @@
+public class Car{
+String model;
+int year;
+public Car(String model,int year){
+this.model=model;
+this.year=year;
+}
+public void displayInfo(){
+System.out.println("Model:"+model+",Year:"+year);
+}
+public static void main(String[]args){
+Car myCar=new Car("Toyota",2021);
+myCar.displayInfo();
+}
+}
