370aa88d971792e0a07a481aa98dff93b84f3a16
keerthivarsha15607-commits <keerthivarsha15607@gmail.com>
2026-02-12 15:13:16 +0530

    Add files via upload

---
+++ OOPS LAB/Person.java
@@ -0,0 +1,16 @@
+public class Person {
+private int age;
+private String name;
+public void setage(int ag){
+age=ag;
+}
+public void setname(String nm){
+name=nm;
+}
+public int getage(){
+return(age);
+}
+public string getname(){
+return(name);
+}
+}
