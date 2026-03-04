0af36b243e0422d832145851584033948452f10f
keerthivarsha15607-commits <keerthivarsha15607@gmail.com>
2026-03-04 23:12:58 +0530

    Add files via upload

---
+++ OOPS LAB/Datypes/BasicDataTypes.java
@@ -0,0 +1,14 @@
+public class BasicDataTypes {
+    public static void main(String[] args) {
+        int number = 10;
+        float decimalNumber = 5.5f; 
+        double bigDecimal = 123.456;
+        char letter = 'A';
+        String text = "Hello Java";
+        System.out.println("Integer value: " + number);
+        System.out.println("Float value: " + decimalNumber);
+        System.out.println("Double value: " + bigDecimal);
+        System.out.println("Character value: " + letter);
+        System.out.println("String value: " + text);
+    }
+}
