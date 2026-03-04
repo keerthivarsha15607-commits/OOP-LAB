6200ae315389edd56ec7aed56949936c7db09661
keerthivarsha15607-commits <keerthivarsha15607@gmail.com>
2026-03-04 19:54:48 +0530

    Add files via upload

---
+++ OOPS LAB/Operators/Operators.java
@@ -0,0 +1,15 @@
+class Operators {
+public static void main(String[] args) {
+int a = 10;
+int b = 3;
+System.out.println("Addition: " + (a + b));
+System.out.println("Subtraction: " + (a - b));
+System.out.println("Multiplication: " + (a * b));
+System.out.println("Division: " + (a / b));
+System.out.println("Modulus: " + (a % b));
+a++;
+System.out.println("Increment: " + a);
+b--;
+System.out.println("Decrement: " + b);
+}
+}
